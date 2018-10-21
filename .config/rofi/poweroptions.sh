1e99867b7bdc0dc789d9ea86575c1de0dfecf2ab
jasperro <jasper.albering@gmail.com>
2018-10-21 20:21:26 +0200

    New gtk3 reload script that requires gnome-settings-daemon

--- .config/rofi/poweroptions.sh
+++ .config/rofi/poweroptions.sh
@@ -1,4 +1,4 @@
-SEL=$(printf "Suspend\nPower Off\nReboot\nLogout\n" | rofi -dmenu -i -p "Select Power Option")
+SEL=$(printf "Suspend\nPower Off\nReboot\nLogout\nSave session\nRestore session" | rofi -dmenu -i -p "Select Power Option")
 
 if [ x"Suspend" = x"${SEL}" ]
 then
@@ -12,4 +12,10 @@ then
 elif [ x"Logout" = x"${SEL}" ]
 then
     i3-msg exit
+elif [ x"Save session" = x"${SEL}" ]
+then
+    ~/session save
+elif [ x"Restore session" = x"${SEL}" ]
+then
+    ~/session restore
 fi
