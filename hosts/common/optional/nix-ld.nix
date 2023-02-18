6b7316c557cf703afe7d7b5b63e09e21b6afc371
Jasper Albering <jasper@albering.nl>
2023-02-18 12:33:17 +0100

    Add more libraries to nix-ld

--- hosts/common/optional/nix-ld.nix
+++ hosts/common/optional/nix-ld.nix
@@ -3,17 +3,54 @@
   # Enable nix ld
   programs.nix-ld.enable = true;
 
-  # Sets up all the libraries to load
+  # Sets up all the libraries to load (From Mic92/dotfiles)
   programs.nix-ld.libraries = with pkgs; [
     stdenv.cc.cc
     zlib
     fuse3
-    icu
-    zlib
-    nss
-    openssl
+    alsa-lib
+    at-spi2-atk
+    at-spi2-core
+    atk
+    cairo
+    cups
     curl
+    dbus
     expat
-    # ...
+    fontconfig
+    freetype
+    gdk-pixbuf
+    glib
+    gtk3
+    libGL
+    libappindicator-gtk3
+    libdrm
+    libnotify
+    libpulseaudio
+    libuuid
+    xorg.libxcb
+    libxkbcommon
+    mesa
+    nspr
+    nss
+    pango
+    pipewire
+    systemd
+    icu
+    openssl
+    xorg.libX11
+    xorg.libXScrnSaver
+    xorg.libXcomposite
+    xorg.libXcursor
+    xorg.libXdamage
+    xorg.libXext
+    xorg.libXfixes
+    xorg.libXi
+    xorg.libXrandr
+    xorg.libXrender
+    xorg.libXtst
+    xorg.libxkbfile
+    xorg.libxshmfence
+    zlib
   ];
 }
