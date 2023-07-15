9adced180e6bc2e225d51924f6bb256dbdb65754
Jasper Albering <jasper@albering.nl>
2023-07-15 21:39:25 +0200

    sops-nix use binary cachix

--- hosts/common/nixos/sops.nix
+++ hosts/common/nixos/sops.nix
@@ -12,4 +12,9 @@ in
   sops = {
     age.sshKeyPaths = map getKeyPath keys;
   };
+
+  nix.settings = {
+    substituters = [ "https://mic92.cachix.org" ];
+    trusted-public-keys = [ "mic92.cachix.org-1:gi8IhgiT3CYZnJsaW7fxznzTkMUOn1RY4GmXdT/nXYQ=" ];
+  };
 }
