77dd54743013dc284f73ca672dd08f57b542d746
Jasper Albering <jasper@albering.nl>
2023-12-20 13:45:52 +0100

    Temporary workaround for EOL obsidian electron version

--- hosts/common/nixos/default.nix
+++ hosts/common/nixos/default.nix
@@ -80,6 +80,9 @@
       allowUnfree = true;
       # Workaround for https://github.com/nix-community/home-manager/issues/2942
       allowUnfreePredicate = (_: true);
+      # Temporary workaround for EOL obsidian electron version
+      permittedInsecurePackages =
+        lib.optional (pkgs.obsidian.version == "1.4.16") "electron-25.9.0";
     };
   };
 
