abcf15b25114e6b93ff208a3c073f35702b7e67b
Jasper Albering <jasper@albering.nl>
2024-03-11 16:40:40 +0100

    Doosje: disable usb wakeup

--- hosts/doosje/services/default.nix
+++ hosts/doosje/services/default.nix
@@ -3,5 +3,19 @@
     ./minecraft
     ../../common/services/podman.nix
   ];
-
+  systemd.services.disable-usb-wakeup = {
+    description = "Disable USB wakeup";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = "yes";
+    };
+    script = ''
+      echo PTXH > /proc/acpi/wakeup
+    '';
+    postStop = ''
+      echo PTXH > /proc/acpi/wakeup
+    '';
+    enable = true;
+  };
 }
