4168ae7fd570c4692d6c69e775f430d3739c65e5
Jasper Albering <jasper@albering.nl>
2024-04-03 10:53:54 +0200

    Some changes that fix some network stuff
    Copied from https://github.com/nix-community/srvos/blob/main/nixos/common/networking.nix

--- hosts/common/nixos/networking.nix
+++ hosts/common/nixos/networking.nix
@@ -2,6 +2,7 @@
 # (e.g. not a container or WSL that are networked separately)
 {
   networking.useNetworkd = true;
+
   services.resolved.enable = true;
 
   services.timesyncd.enable = true;
@@ -22,4 +23,21 @@
     };
     openFirewall = true;
   };
+
+  # Some changes that fix some network stuff
+  # Copied from https://github.com/nix-community/srvos/blob/main/nixos/common/networking.nix
+
+  # The notion of "online" is a broken concept
+  # https://github.com/systemd/systemd/blob/e1b45a756f71deac8c1aa9a008bd0dab47f64777/NEWS#L13
+  systemd.services.NetworkManager-wait-online.enable = false;
+  systemd.network.wait-online.enable = false;
+
+  # FIXME: Maybe upstream?
+  # Do not take down the network for too long when upgrading,
+  # This also prevents failures of services that are restarted instead of stopped.
+  # It will use `systemctl restart` rather than stopping it with `systemctl stop`
+  # followed by a delayed `systemctl start`.
+  systemd.services.systemd-networkd.stopIfChanged = false;
+  # Services that are only restarted might be not able to resolve when resolved is stopped before
+  systemd.services.systemd-resolved.stopIfChanged = false;
 }
