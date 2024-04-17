8bca96122602c2e653d50426c78f1501afb5a5c6
Jasper Albering <jasper@albering.nl>
2024-04-17 10:11:42 +0200

    Fix breaking changes

--- hosts/common/desktop/kde-sddm-wayland.nix
+++ hosts/common/desktop/kde-sddm-wayland.nix
@@ -9,18 +9,18 @@
         variant = "altgr-intl";
         options = "terminate:ctrl_alt_bksp";
       };
-      displayManager = {
-        sddm = {
-          enable = true;
-          wayland.enable = true;
-          settings = {
-            Theme = { CursorTheme = "breeze_cursors"; };
-          };
-        };
-        defaultSession = "plasma";
-      };
     };
     desktopManager.plasma6.enable = true;
+    displayManager = {
+      sddm = {
+        enable = true;
+        wayland.enable = true;
+        settings = {
+          Theme = { CursorTheme = "breeze_cursors"; };
+        };
+      };
+      defaultSession = "plasma";
+    };
   };
 
   networking.networkmanager.enable = true;
