622be47ff7dc94db052567d5628d328a47353b14
Jasper Albering <jasper@albering.nl>
2024-06-04 13:12:54 +0200

    Update nixpkgs from 24.05 to 24.11
    flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'hardware':
        'github:nixos/nixos-hardware/d9e0b26202fd500cf3e79f73653cce7f7d541191' (2024-05-20)
      → 'github:nixos/nixos-hardware/7b49d3967613d9aacac5b340ef158d493906ba79' (2024-06-01)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/e4611630c3cc8ed618b48d92f6291f65be9f7913' (2024-05-25)
      → 'github:nix-community/home-manager/2cacdd6a27477f1fa46b7026dd806de30f164d3b' (2024-06-04)
    • Updated input 'nix-minecraft':
        'github:Misterio77/nix-minecraft/a43548e8574c8e4aa196c5c8ceb38cb3c3e14457' (2024-05-25)
      → 'github:Misterio77/nix-minecraft/322f3ede28cf8da3faedbfc3afc4582e7734a3ad' (2024-06-04)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/bfb7a882678e518398ce9a31a881538679f6f092' (2024-05-24)
      → 'github:nixos/nixpkgs/57610d2f8f0937f39dbd72251e9614b1561942d8' (2024-05-31)
    • Updated input 'nixpkgs-stable':
        'github:nixos/nixpkgs/46397778ef1f73414b03ed553a3368f0e7e33c2f' (2024-05-22)
      → 'github:nixos/nixpkgs/b3b2b28c1daa04fe2ae47c21bb76fd226eac4ca1' (2024-06-01)
    • Updated input 'nixpkgs-unstable':
        'github:nixos/nixpkgs/bfb7a882678e518398ce9a31a881538679f6f092' (2024-05-24)
      → 'github:nixos/nixpkgs/57610d2f8f0937f39dbd72251e9614b1561942d8' (2024-05-31)
    • Updated input 'nixpkgs-unstable-small':
        'github:nixos/nixpkgs/6de51d98ec2ae46730f11845e221aab9d2470a8a' (2024-05-25)
      → 'github:nixos/nixpkgs/518daacfe2ee732432b4a7dac19b72eda037d44d' (2024-06-03)
    • Updated input 'nixvim':
        'github:nix-community/nixvim/1cc2e02fcaabd224348fa0dbfeb311063787a060' (2024-05-25)
      → 'github:nix-community/nixvim/b7a8b0319098fdbaa719ef4dc375337ec4543c6e' (2024-06-03)
    • Added input 'nixvim/git-hooks':
        'github:cachix/git-hooks.nix/0e8fcc54b842ad8428c9e705cb5994eaf05c26a0' (2024-05-20)
    • Added input 'nixvim/git-hooks/flake-compat':
        'github:edolstra/flake-compat/0f9255e01c2351cc7d116c072cb317785dd33b33' (2023-10-04)
    • Added input 'nixvim/git-hooks/gitignore':
        'github:hercules-ci/gitignore.nix/637db329424fd7e46cf4185293b9cc8c88c95394' (2024-02-28)
    • Added input 'nixvim/git-hooks/gitignore/nixpkgs':
        follows 'nixvim/git-hooks/nixpkgs'
    • Added input 'nixvim/git-hooks/nixpkgs':
        follows 'nixvim/nixpkgs'
    • Added input 'nixvim/git-hooks/nixpkgs-stable':
        follows 'nixvim/nixpkgs'
    • Updated input 'nixvim/nix-darwin':
        'github:lnl7/nix-darwin/0bea8222f6e83247dd13b055d83e64bce02ee532' (2024-05-24)
      → 'github:lnl7/nix-darwin/c0d5b8c54d6828516c97f6be9f2d00c63a363df4' (2024-05-29)
    • Removed input 'nixvim/pre-commit-hooks'
    • Removed input 'nixvim/pre-commit-hooks/flake-compat'
    • Removed input 'nixvim/pre-commit-hooks/gitignore'
    • Removed input 'nixvim/pre-commit-hooks/gitignore/nixpkgs'
    • Removed input 'nixvim/pre-commit-hooks/nixpkgs'
    • Removed input 'nixvim/pre-commit-hooks/nixpkgs-stable'
    • Updated input 'nur':
        'github:nix-community/NUR/5a9cefff93fe9a7e8d864d48ea0fba144afef2a6' (2024-05-26)
      → 'github:nix-community/NUR/ab534f14c307f729de6954e36837e9febdbda97e' (2024-06-04)
    • Updated input 'sops-nix':
        'github:mic92/sops-nix/b549832718b8946e875c016a4785d204fcfc2e53' (2024-05-22)
      → 'github:mic92/sops-nix/d4555e80d80d2fa77f0a44201ca299f9602492a0' (2024-06-03)

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = rec {
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-unstable-small.url = "github:nixos/nixpkgs/nixos-unstable-small";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
 
     nixpkgs = nixpkgs-unstable;
 
