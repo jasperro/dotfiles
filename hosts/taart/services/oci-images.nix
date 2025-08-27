720afbf1529b1fd2eb05a0b42709bb5ff5dba958
Jasper Albering <jasper@albering.nl>
2025-08-27 21:42:27 +0200

    Update OCI Images

--- hosts/taart/services/oci-images.nix
+++ hosts/taart/services/oci-images.nix
@@ -5,15 +5,15 @@
 {
   home-assistant = {
     imageName = "ghcr.io/home-assistant/home-assistant";
-    imageDigest = "sha256:90e105ff097717556df4e87da3b825af85b181c763ca2b8d840aeae5d34a083c";
-    hash = "sha256-Q5krvVAHMVVmc4MAPcJ224GUCXZLFYPXStKBAiWorSc=";
+    imageDigest = "sha256:2eb83307a08de3287822b648fce4cbf1364423b55f2af623c149c6b21c2cef00";
+    hash = "sha256-H+g4W0sMoTat6VKQL4rUWhyLIW2f4lU0GO7nadFVgUE=";
     finalImageName = "ghcr.io/home-assistant/home-assistant";
     finalImageTag = "latest";
   };
   appdaemon = {
     imageName = "acockburn/appdaemon";
-    imageDigest = "sha256:55bfe7c9d43a18ae3ac2bcf374cf1d64921dab351f0cc8979a65153dbbcd399f";
-    hash = "sha256-Dq035k3zke+akdmuCAQ/UMKAt3AOrS6YuCPhL+i3Kxs=";
+    imageDigest = "sha256:512c86e8cc24a3654e4cd2ddaf8a64ecd4e3707976a66b85bc2d9be51553be67";
+    hash = "sha256-DCpCTjx9OFJlSQ4gEODXrE3awjmNNwM7IGBlI7ua7nU=";
     finalImageName = "acockburn/appdaemon";
     finalImageTag = "latest";
   };
