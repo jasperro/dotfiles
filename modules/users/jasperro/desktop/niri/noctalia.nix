f8751a48814f25dd563a165f162e0d01849f0c47
Jasper Albering <jasper@albering.nl>
2026-07-11 00:05:13 +0200

    Add extra noctalia stuff

--- modules/users/jasperro/desktop/niri/noctalia.nix
+++ modules/users/jasperro/desktop/niri/noctalia.nix
@@ -8,6 +8,24 @@
   };
 
   JDF.users._.jasperro._.desktop._.niri._.noctalia = {
+    nixos = {
+      key = "jasperro-noctalia";
+      imports = [
+        inputs.noctalia.nixosModules.default
+      ];
+
+      nix.settings = {
+        extra-substituters = [ "https://noctalia.cachix.org" ];
+        extra-trusted-public-keys = [
+          "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+        ];
+      };
+
+      programs.noctalia = {
+        enable = true;
+        recommendedServices.enable = true;
+      };
+    };
     homeManager = {
       key = "jasperro-noctalia";
       imports = [
