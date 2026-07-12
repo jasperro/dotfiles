f646298716421257f9614c95d8a62f7ba44ee195
Jasper Albering <jasper@albering.nl>
2026-07-12 13:27:57 +0200

    Temporarily add back pyPatchNixpkgs for nix-alien

--- flake.nix
+++ flake.nix
@@ -24,11 +24,12 @@
       url = "git+https://codeberg.org/BANanaD3V/niri-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    pyPatchNixpkgs.url = "github:applePrincess/nixpkgs/fix-dpcontracts";
     nix-alien = {
       url = "github:thiagokokada/nix-alien";
       inputs = {
         flake-compat.follows = "flake-compat";
-        nixpkgs.follows = "nixpkgs";
+        nixpkgs.follows = "pyPatchNixpkgs";
       };
     };
     nixos-wsl = {
